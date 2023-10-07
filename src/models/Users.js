b726501d0240e1d6450fd23741dbd7efbf849fb8
dineshkumar-stack <stardinesh4@gmail.com>
2023-10-07 16:26:57 +0530

    setup

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,35 +1,17 @@
-// const mongoose = require("mongoose"); // Require mongoose package and store it in a variable
+const { Schema, model } = require("mongoose");
 
-// const UserSchema = new mongoose.Schema({ // Create a schema: Schema function which is from mongoose takes one parameter, an object
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     },
-//     email: {
-//         type: String
-//     }
-// });
-
-// const User = mongoose.model("users", UserSchema) // Create a model: model function takes two paramaters what and how
-
-// module.exports = User; // Export model
-
-const {Schema, model} = require("mongoose"); // Require mongoose package and store it in a variable
-
-const UserSchema = new Schema({ // Create a schema: Schema function which is from mongoose takes one parameter, an object
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    },
-    email: {
-        type: String
-    }
+const UserSchema = new Schema({
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  email: {
+    type: String,
+  },
 });
 
-const User = model("users", UserSchema) // Create a model: model function takes two paramaters what and how
+const User = model("users", UserSchema);
 
-module.exports = User; // Export model
+module.exports = User;
